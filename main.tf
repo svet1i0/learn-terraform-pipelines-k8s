355f3060278b0fc2b66f7168da8bc6ce5480343a
svet1i0 <75417040+svet1i0@users.noreply.github.com>
2021-04-07 14:46:52 +0300

    Update main.tf
    added VPC & Subnet

--- main.tf
+++ main.tf
@@ -21,3 +21,17 @@ provider "google" {
   project = var.google_project
   region  = var.region
 }
+
+# VPC
+resource "google_compute_network" "vpc" {
+  name                    = "${var.google_project}-vpc"
+  auto_create_subnetworks = "false"
+}
+
+# Subnet
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.google_project}-subnet"
+  region        = var.region
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.10.0.0/24"
+}
